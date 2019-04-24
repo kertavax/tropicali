dbbef58ded0d85eb8bd88e5bf529b374e7cd0cc7
Egill R Vidarsson <egill@hvitahusid.is>
2019-04-24 10:28:04 +0000

    added gulp-clean-css, gulp-sourcemaps and browser-sync to gulp tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,32 @@
 var gulp = require('gulp')
 var sass = require('gulp-sass')
+var clean_css = require('gulp-clean-css')
+var sourcemaps = require('gulp-sourcemaps')
+var browser_sync = require('browser-sync').create()
+
 sass.compiler = require('node-sass')
 
-gulp.task('default', function() {
-	// we want to run sass 'css/style.scss style.css'
+gulp.task('sass', function() {
 	return gulp.src('./css/style.scss')
+		.pipe(sourcemaps.init())
 		.pipe(sass())
+		.pipe(
+			clean_css({
+				compatibility: 'ie8'
+			})
+		)
+		.pipe(sourcemaps.write())
 		.pipe(gulp.dest('./css'))
+		.pipe(browser_sync.stream())
 })
+
+gulp.task('watch', function() {
+	browser_sync.init({
+		server: {
+			baseDir: './'
+		}
+	})
+	gulp.watch('./css/style.scss', ['sass'])
+})
+
+gulp.task('default', ['sass', 'watch'])
