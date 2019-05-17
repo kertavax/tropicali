7a837d89ae7133c2b6fedc90fe28708a794a4db9
Egill R Vidarsson <egill@hvitahusid.is>
2019-05-17 14:56:40 +0000

    rearranged structure into dist and src format. created images and fonts gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,51 @@
-var gulp = require('gulp')
-var sass = require('gulp-sass')
-var clean_css = require('gulp-clean-css')
-var sourcemaps = require('gulp-sourcemaps')
-var browser_sync = require('browser-sync').create()
+var gulp = require("gulp")
+var sass = require("gulp-sass")
+var clean_css = require("gulp-clean-css")
+var sourcemaps = require("gulp-sourcemaps")
+var browser_sync = require("browser-sync").create()
 
-sass.compiler = require('node-sass')
+sass.compiler = require("node-sass")
 
-gulp.task('sass', function() {
-	return gulp.src('./css/style.scss')
+gulp.task("sass", function() {
+	return gulp.src("src/css/style.scss")
 		.pipe(sourcemaps.init())
 		.pipe(sass())
 		.pipe(
 			clean_css({
-				compatibility: 'ie8'
+				compatibility: "ie8"
 			})
 		)
 		.pipe(sourcemaps.write())
-		.pipe(gulp.dest('./css'))
+		.pipe(gulp.dest("dist"))
 		.pipe(browser_sync.stream())
 })
 
-gulp.task('watch', function() {
-	browser_sync.init({
-		server: {
-			baseDir: './'
-		}
-	})
-	gulp.watch('./css/style.scss', ['sass'])
+gulp.task("html", function() {
+	return gulp.src("src/index.html")
+		.pipe(gulp.dest("dist"))
 })
 
-gulp.task('default', ['sass', 'watch'])
+gulp.task("fonts", function() {
+	return gulp.src("src/fonts/*")
+		.pipe(gulp.dest("dist/fonts"))
+})
+
+gulp.task("images", function() {
+	return gulp.src("src/img/*")
+		.pipe(gulp.dest("dist/img"))
+})
+
+gulp.task("watch", function() {
+	browser_sync.init({
+		server: {
+			baseDir: "dist"
+		}
+	})
+
+	gulp.watch("src/index.html", ["html"]).on("change", browser_sync.reload)
+	gulp.watch("src/css/style.scss", ["sass"])
+	gulp.watch("src/fonts/*", ["fonts"])
+	gulp.watch("src/img/*", ["images"])
+})
+
+gulp.task("default", ["html", "sass", "fonts", "images", "watch"])
